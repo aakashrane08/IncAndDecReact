5c29ef6e57bae4588a6690e2658a2620f6c0b746
aakashrane08 <aakash.rane.008@gmail.com>
2023-06-15 19:32:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function decHandler() {
+    setCount(count - 1);
+  }
+  function incHandler() {
+    setCount(count + 1);
+  }
+  function resetHandler() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-gray-700 flex-col gap-4">
+      <div className="font-medium text-blue-400 text-2xl">
+        Increment && Decrement
+      </div>
+
+      <div className="flex gap-10 bg-white px-3 py-2 rounded text-[25px] text-gray-700 justify-center">
+        <button
+          onClick={decHandler}
+          className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">{count}</div>
+        <button
+          onClick={incHandler}
+          className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          +
+        </button>
+      </div>
+      
+      <button onClick={resetHandler} className="text-white bg-blue-500 py-2 px-5 rounded text-lg">
+        Reset
+      </button>
     </div>
   );
 }
